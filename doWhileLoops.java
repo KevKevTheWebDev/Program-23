463cbb1d553e9d2e8c97e96d9bf41b5e53a0644e
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-21 18:30:57 -0400

    Update and rename README.md to doWhileLoops.java

---
+++ doWhileLoops.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class apples{
+	public static void main(String args[]){
+		int counter = 15;
+		do{
+			System.out.println("Meow!");
+			counter++;
+		}while(counter<=10);
+	}
+}
